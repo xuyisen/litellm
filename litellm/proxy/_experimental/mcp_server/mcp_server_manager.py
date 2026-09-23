0bf70e90939e01af1fa00dc95e0dcaa1fbb23670
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-22 21:26:54 -0400

    xuyisen__diff__id_379

--- litellm/proxy/_experimental/mcp_server/mcp_server_manager.py
+++ litellm/proxy/_experimental/mcp_server/mcp_server_manager.py
@@ -815,7 +815,7 @@ class MCPServerManager:
         resource_metadata_url = params.get("resource_metadata")
 
         scope_value = params.get("scope")
-        scopes = [s for s in (scope_value.split() if scope_value else []) if s]
+        scopes: Optional[List[str]] = [s for s in (scope_value.split() if scope_value else []) if s]
         scopes = scopes or None
 
         return resource_metadata_url, scopes
